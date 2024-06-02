0809acbcba000eaf1a948d669e1894e90d9aacfc
T2K-UK <tom.ricketts12345@gmail.com>
2024-06-02 18:05:29 +0100

    remove section 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: "/pikaonsol",
+    output: 'export',  // enables static exports
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,  // Disable the Image Optimization API
+    },
+  };
+  
+  export default nextConfig;
