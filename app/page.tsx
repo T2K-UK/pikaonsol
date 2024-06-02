42811f82583b3d653efc6b6a8c7a3a0bb1ad3cb1
T2K-UK <tom.ricketts12345@gmail.com>
2024-06-02 18:10:38 +0100

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Sec1 from "@/components/Section1";
 import Sec2 from "@/components/Section2";
-import Sec3 from "@/components/Section3";
-import Sec4 from "@/components/Section4";
-import Sec5 from "@/components/Section5";
+
 
 export default function Home() {
   return (
