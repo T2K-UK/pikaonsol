9575709673713022e4ed8d8ca38f935b13513bc6
T2K-UK <tom.ricketts12345@gmail.com>
2024-06-02 21:00:52 +0100

    FINALLY FIXED

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-
-const isProd = process.env.NODE_ENV = 'production'
-
-
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? "/pikaonsol" : '',
-    output: 'export',  // enables static exports
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,  // Disable the Image Optimization API
-    },
-  };
-  
-  export default nextConfig;
+  output: 'export',  // enables static exports
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,  // Disable the Image Optimization API
+  },
+  webpack: (config, { isServer }) => {
+    // Custom Webpack configuration (without custom CSS loaders)
+    return config;
+  },
+};
+
+export default nextConfig;
